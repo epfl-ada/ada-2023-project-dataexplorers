8c0dce09a02f6b6623469f674be8f5b5a7c3248e
Anton Balykov <49350081+CaptainShepard01@users.noreply.github.com>
2023-11-15 09:20:48 +0100

    Added an argument for chrome_options to launch Chrome in French language for correct scraping

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -12,6 +12,7 @@ class ImdbScraper:
         Initializes the ImdbScraper class
         """
         chrome_options  = webdriver.ChromeOptions()
+        chrome_options.add_argument("--lang=fr")
         self.driver     = webdriver.Chrome(options=chrome_options)
     
     def close(self):
